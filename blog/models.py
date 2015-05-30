71fd165d06cfcd20f425c6181e16d2344ae01722
volkan <volkan.kaya@hotmail.co.uk>
2015-05-30 18:11:43 +0100

    v2

--- blog/models.py
+++ blog/models.py
@@ -13,5 +13,23 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved=True)
+
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved = True
+        self.save()
+
+    def __str__(self):
+        return self.text
--- mysite/settings.py
+++ mysite/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'mysite.urls'
+LOGIN_REDIRECT_URL = '/'
 
 TEMPLATES = [
     {
